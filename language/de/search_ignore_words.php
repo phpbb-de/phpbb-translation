decee99a3ce1fde518118f71f8f9a6aabdcd45ca
Philipp Kordowich <philipp@phpbb.de>
2014-04-26 09:09:57 +0200

    search_ignore_words.php und search_synonyms.php überführt

--- language/de/search_ignore_words.php
+++ language/de/search_ignore_words.php
@@ -4,7 +4,7 @@
 * search_ignore_words [Deutsch — Du]
 *
 * @package language
-* @copyright (c) 2005 phpBB Group; 2006 phpBB.de
+* @copyright (c) 2005 phpBB Group; 2006-2014 phpBB.de
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
 *
 * Deutsche Übersetzung durch die Übersetzer-Gruppe von phpBB.de:
