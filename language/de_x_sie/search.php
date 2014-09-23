4106cbee0e8ae619ac4233e73fca1666a1d7f0f6
Philipp Kordowich <philipp@phpbb.de>
2014-09-23 20:03:35 +0200

    Fehlende Anpassung der Variable RETURN_TO_SEARCH_ADV

--- language/de_x_sie/search.php
+++ language/de_x_sie/search.php
@@ -89,7 +89,7 @@ $lang = array_merge($lang, array(
 	'RESULT_DAYS'			=> 'Suchzeitraum begrenzen',
 	'RESULT_SORT'			=> 'Ergebnisse sortieren nach',
 	'RETURN_FIRST'			=> 'Die ersten',
-	'RETURN_TO_SEARCH_ADV'	=> 'Zur erweiterten Suche',
+	'GO_TO_SEARCH_ADV'	=> 'Zur erweiterten Suche',
 
 	'SEARCHED_FOR'				=> 'Benutzte Suchanfrage',
 	'SEARCHED_TOPIC'			=> 'Durchsuchtes Thema',
