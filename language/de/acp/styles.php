38fa0f412653d5ee88f9aa43e43000ef62135739
Martin Beckmann <gn#36@phpbb.de>
2015-08-23 19:00:32 +0200

    Update "de" auf 3.1.6

--- language/de/acp/styles.php
+++ language/de/acp/styles.php
@@ -86,4 +86,6 @@ $lang = array_merge($lang, array(
 	'STYLE_USED_BY'				=> 'Verwendet von (inklusive Spiders/Robots)',
 
 	'UNINSTALL_DEFAULT'		=> 'Der Standard-Style kann nicht deinstalliert werden.',
+
+	'BROWSE_LANGUAGE_PACKS_DATABASE'	=> 'Styledatenbank durchsuchen',
 ));
